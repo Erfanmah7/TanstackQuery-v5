85570ead0aa587cb58ccd315c3435e3d25492f75
Erfanmah7 <efn8mah@yahoo.com>
2024-08-02 17:26:03 +0330

    useMutation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 function App() {
@@ -9,15 +9,38 @@ function App() {
 
   // gcTime = cashTime
   //isLoading => isPending && isFetching
-  const { data, isPending } = useQuery({
-    queryKey,
-    queryFn,
-    gcTime: 50000,
-    refetchOnWindowFocus: false,
-  });
-  console.log({ data, isPending });
+  // const { data, isPending } = useQuery({
+  //   queryKey,
+  //   queryFn,
+  //   gcTime: 50000,
+  //   refetchOnWindowFocus: false,
+  // });
 
-  return <>app</>;
+  // console.log({ data, isPending });
+
+  const mutationFn = (data) => {
+    return axios.post("https://jsonplaceholder.typicode.com/posts", data);
+  };
+
+  const clickHandler = () => {
+    const data = {
+      name: "erfan",
+    };
+
+    mutate(data, {
+      onError: () => console.log("error", error),
+      onSuccess: (data) => console.log("data", data),
+    });
+  };
+  const { mutate, isPending } = useMutation({ mutationFn });
+
+  return (
+    <>
+      <div>
+        <button onClick={clickHandler}>Send</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
